d2a8545df2a386dd365b6f2babf21490934c07be
Guillem Francisco <guillemfrancisco@gmail.com>
2018-08-10 12:34:51 +0200

    Missing things in tracker.py

--- tracker.py
+++ tracker.py
@@ -13,6 +13,8 @@
 #-----------------------------
 import numpy as np
 import utils
+import cv2
+import pickle
 from kalman_filter import KalmanFilter
 from common import dprint
 from scipy.optimize import linear_sum_assignment
@@ -99,7 +101,7 @@ class Tracker(object):
             for i in range(len(detections)):
                 bbox = detections[i][:4]
                 center = utils.bboxToCenter(bbox)
-                type = detections[4]
+                type = detections[i][4]
                 track = Track(center, bbox, type)
                 self.tracks.append(track)
 
@@ -207,7 +209,6 @@ class Tracker(object):
             xmin, ymin, xmax, ymax = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
             pt1 = (xmin, ymin)
             pt2 = (xmax, ymax)
-            if(len(bbox) == 6):
-                otype = list(YOLOdict.keys())[list(YOLOdict.values()).index(self.tracks[i].TYPE)]
-                cv2.rectangle(img, pt1, pt2, color, 1)
-                cv2.putText(img, otype+str(self.tracks[i].ID), (pt1[0], pt1[1] + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 6)
+            otype = list(YOLOdict.keys())[list(YOLOdict.values()).index(self.tracks[i].TYPE)]
+            cv2.rectangle(img, pt1, pt2, color, 1)
+            cv2.putText(img, otype+str(self.tracks[i].ID), (pt1[0], pt1[1] + 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 6)
